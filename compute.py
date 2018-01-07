e05b86b28c6ae44a798a996dc1abd7f93878ad8d
safwen <baroudi.safwene@gmail.com>
2018-01-07 16:52:25 +0100

    Add information about individiual payments

--- compute.py
+++ compute.py
@@ -18,22 +18,30 @@ for i in range(len(content_list)):
         continue
     D[content_list[i].split('=')[0]]=content_list[i].split('=')[1]
 
+paid_by_safwen=Decimal(0)
+paid_by_fadhel=Decimal(0)
 safwen=Decimal(0)
 fadhel=Decimal(0)
 total_commun=Decimal(0)
 
 for key in D:
     if key[0] == 'S':
+        paid_by_safwen = paid_by_safwen + Decimal(D[key])
+    else:
+        if key[0] == 'F':
+            paid_by_fadhel = paid_by_fadhel + Decimal(D[key])
+
+    if key[1] == 'S':
         safwen = safwen + Decimal(D[key])
     else :
-        if key[0] == 'F':
+        if key[1] == 'F':
             fadhel = fadhel + Decimal(D[key])
         else:
-            if key[0] == 'C':
+            if key[1] == 'C':
                 total_commun = total_commun + Decimal(D[key])
             else:
                 print("Frais est attribué à un inconnu.")
-                print("Vérifie la première lettre de la ligne contenant: ", D[key])
+                print("Vérifie la seconde lettre de la ligne contenant: ", D[key])
 
 print("safwen: %.2f" % safwen)
 print("fadhel: %.2f" % fadhel)
@@ -41,3 +49,6 @@ print("commun: %.2f" % total_commun)
 print("total: %.2f" % (safwen + fadhel + total_commun))
 print("total safwen: %.2f" % (safwen + total_commun/Decimal(2)))
 print("total fadhel: %.2f" % (fadhel + total_commun/Decimal(2)))
+print("Safwen a payé: %.2f" % paid_by_safwen)
+print("Fadhel a payé: %.2f" % paid_by_fadhel)
+print("total a payé: %.2f" % (paid_by_safwen+paid_by_fadhel))
